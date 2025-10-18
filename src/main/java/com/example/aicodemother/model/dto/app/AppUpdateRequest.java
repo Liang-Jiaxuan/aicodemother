00f995601b0511b136c0dc210c5cef1b5f87e1b9
Liang-Jiaxuan <1617929300@qq.com>
2025-10-18 15:35:10 +0800

    应用模块(完成基本功能) 1. AI 生成基础功能代码 2. 完成基本功能, 人工测试通过

---
+++ src/main/java/com/example/aicodemother/model/dto/app/AppUpdateRequest.java
@@ -0,0 +1,24 @@
+package com.example.aicodemother.model.dto.app;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 更新应用请求, 普通用户只能更新 id 和 应用名称
+ */
+@Data
+public class AppUpdateRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 应用名称
+     */
+    private String appName;
+
+    private static final long serialVersionUID = 1L;
+}
--- src/main/java/com/example/aicodemother/service/AppService.java
+++ src/main/java/com/example/aicodemother/service/AppService.java
@@ -1,13 +1,44 @@
 package com.example.aicodemother.service;
 
+import com.example.aicodemother.model.dto.app.AppQueryRequest;
+import com.example.aicodemother.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.example.aicodemother.model.entity.App;
 
+import java.util.List;
+
 /**
- *  服务层。
+ * 服务层。
  *
  * @author 梁家萱
  */
 public interface AppService extends IService<App> {
 
+    /**
+     * 获取应用封装类
+     *
+     * @param app 应用实体类
+     * @return 应用封装类
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取应用封装类列表
+     *
+     * @param appList 应用实体类列表
+     * @return 应用封装类列表
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     * 将 AppQueryRequest 这个 dto
+     * 转换为
+     * MyBatis Flex 查询所需要的 QueryWrapper
+     *
+     * @param appQueryRequest 应用查询请求
+     * @return 应用查询条件
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 }
